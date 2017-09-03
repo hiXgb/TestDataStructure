eb5b949ee139f47553904f61db75736b6ad50329
xgb <xieguobiyi@gmail.com>
2017-09-03 21:06:14 +0800

    Initial Commit

---
+++ TestDataStructure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDataStructure
+//
+//  Created by xgb on 2017/9/3.
+//  Copyright © 2017年 xgb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
